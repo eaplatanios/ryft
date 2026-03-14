db8c21992ab8abe1ce9a05e27fc4fd1f02d79224
Anthony Platanios <e.a.platanios@gmail.com>
2026-03-13 22:59:57 -0700

    .

--- crates/ryft-core/src/types/mod.rs
+++ crates/ryft-core/src/types/mod.rs
@@ -1,10 +1,12 @@
-use std::fmt::Display;
-
 pub mod data_type;
 
-// TODO(eaplatanios): [DOC].
-// TODO(eaplatanios): Should [`Display`] really be a requirement here?
-pub trait Type: Display {
+pub use data_type::*;
+
+/// Lightweight type-level description of a family of runtime values. A [`Type`] captures the structural metadata that
+/// Ryft needs to reason about values without inspecting the values themselves. Examples include scalar data types such
+/// as [`DataType`], array-like types that combine an element [`DataType`] with shape information, and richer type
+/// descriptors for traced values.
+pub trait Type {
     /// Returns `true` if this [`Type`] is a subtype of the provided [`Type`] (i.e., when
     /// values that belong to this [`Type`] can be safely cast to the provided [`Type`]).
     fn is_subtype_of(&self, other: &Self) -> bool;
