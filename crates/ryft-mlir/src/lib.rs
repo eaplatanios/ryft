6bb7e569305e782850c44ad6815b92721c0ae92d
Anthony Platanios <e.a.platanios@gmail.com>
2026-05-05 20:40:42 -0700

    .

--- crates/ryft-mlir/src/lib.rs
+++ crates/ryft-mlir/src/lib.rs
@@ -9,6 +9,7 @@ pub mod blocks;
 pub mod context;
 pub mod diagnostics;
 pub mod dialects;
+pub mod errors;
 pub mod execution_engine;
 pub mod identifier;
 pub mod locations;
@@ -52,6 +53,7 @@ pub use self::dialects::builtin::{
     UnrealizedConversionCastOperationRef, VectorTypeDimension, VectorTypeRef,
 };
 pub use self::dialects::{Dialect, DialectHandle, DialectRegistry};
+pub use self::errors::*;
 pub use self::execution_engine::*;
 pub use self::identifier::*;
 pub use self::locations::*;
